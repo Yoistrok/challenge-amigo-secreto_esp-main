e8750ed70400155786b1e2901e44a630470b830f
Daniel Huerta <yoistrok@gmail.com>
2025-02-01 15:58:35 -0600

    Se logra que la lista se actualice

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-console.log('Huevos')
+let nombresAmigos = [];
+console.log(nombresAmigos);
+
+function agregarAmigoLista(){ //al hace clic en el botón
+    let inputIngresado = document.getElementById('amigo').value; //se declara una variable, que tomará el valor de lo ingresado en el input 
+    console.log(inputIngresado);// se imprime el valor obtenido
+    nombresAmigos.push(inputIngresado);
+    console.log(nombresAmigos);
+    return; // se logra ingresar los nombres a la lista 
+}
+
+
